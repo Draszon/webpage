82c8613b3ba3103e2cdfa600dc4cf9742c110591
Draszon <peti0202@windowslive.com>
2024-09-06 18:46:58 +0200

    kő papír olló optimalizálása

--- projects/rockpaperscissor.js
+++ projects/rockpaperscissor.js
@@ -1,89 +1,32 @@
-const playerScoreDisplay = document.getElementById('player-score');
-const pcScoreDisplay = document.getElementById('pc-score');
+"use strict";
 
-const pcRock = document.getElementById('pc-rock');
-const pcPaper = document.getElementById('pc-paper');
-const pcScissor = document.getElementById('pc-scissor');
+const game = {
+    data: {
+        playerScoreDisplay: document.getElementById("player-score"),
+        pcScoreDisplay: document.getElementById("pc-score"),
+        result: document.getElementById("result"),
+        playerFields: document.querySelectorAll(".player-icons"),
+        pcSore: 0,
+        playerScore: 0
+    },
 
-const playerRock = document.getElementById('player-rock');
-const playerPaper = document.getElementById('player-paper');
-const playerScissor = document.getElementById('player-scissor');
+    random() {
+        return Math.floor(Math.random() *3);
+    },
 
-const resultText = document.getElementById('result');
+    winLooseCheck(fieldIndex) {
+        
+    },
 
-let playerScoreCounter = 0;
-let pcScoreCounter = 0;
-
-function pcScoreGenerator() {
-    const value = Math.floor(Math.random() * (4 - 1) +1);
-    return value;
-}
-
-function pcHighlightIcon(pcScoreValue) {
-    switch(pcScoreValue) {
-        case 1:
-            pcRock.style.color = 'white';
-            break;
-        case 2:
-            pcPaper.style.color = 'white';
-            break;
-        case 3:
-            pcScissor.style.color = 'white';
-            break;
+    init() {
+        this.data.playerFields.forEach(field =>  {
+            field.addEventListener("click", () => {
+                this.data.pcSore = this.random();
+                let fieldIndex = parseInt(field.dataset.index, 10);
+                this.winLooseCheck(fieldIndex);
+            });
+        });
     }
-}
+};
 
-function winLooseCheck(pcScore, playerScore) {
-    
-    if(pcScore === 1 && playerScore === 1 || pcScore === 2 && playerScore === 2 || pcScore === 3 && playerScore === 3) {
-        resultText.textContent = 'Döntetlen!';
-    } else if(playerScore === 1 && pcScore === 3 || playerScore === 2 && pcScore === 1 || playerScore === 3 && pcScore === 2) {
-        resultText.textContent = 'Gratulálok nyertél!!';
-        playerScoreCounter += 1;
-        playerScoreDisplay.textContent = playerScoreCounter;
-    } else if(playerScore === 1 && pcScore === 2 || playerScore === 2 && pcScore === 3 || playerScore === 3 && pcScore === 1) {
-        resultText.textContent = 'A gép nyert!';
-        pcScoreCounter += 1;
-        pcScoreDisplay.textContent = pcScoreCounter;
-    }
-}
-
-function boardClear() {
-    setTimeout(function() {
-        playerRock.style.color = 'hsl(0, 0%, 60%)';
-        playerPaper.style.color = 'hsl(0, 0%, 60%)';
-        playerScissor.style.color = 'hsl(0, 0%, 60%)';
-
-        pcRock.style.color = 'hsl(0, 0%, 60%)';
-        pcPaper.style.color = 'hsl(0, 0%, 60%)';
-        pcScissor.style.color = 'hsl(0, 0%, 60%)';
-
-        resultText.textContent = '';
-    }, 1500);
-}
-
-
-
-playerRock.addEventListener('click', () => {
-    let pcScore = pcScoreGenerator();
-    pcHighlightIcon(pcScore);
-    playerRock.style.color = 'white';
-    winLooseCheck(pcScore, 1);
-    boardClear();
-});
-
-playerPaper.addEventListener('click', () => {
-    let pcScore = pcScoreGenerator();
-    pcHighlightIcon(pcScore);
-    playerPaper.style.color = 'white';
-    winLooseCheck(pcScore, 2);
-    boardClear();
-});
-
-playerScissor.addEventListener('click', () => {
-    let pcScore = pcScoreGenerator();
-    pcHighlightIcon(pcScore);
-    playerScissor.style.color = 'white';
-    winLooseCheck(pcScore, 3);
-    boardClear();
-});
+game.init();
