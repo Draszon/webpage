89c434ad0d2be8c73c026b763e9b504f6218a151
Draszon <peti0202@windowslive.com>
2024-09-09 00:08:03 +0200

    kő papír olló haladás

--- projects/rockpaperscissor.js
+++ projects/rockpaperscissor.js
@@ -6,6 +6,7 @@ const game = {
         pcScoreDisplay: document.getElementById("pc-score"),
         result: document.getElementById("result"),
         playerFields: document.querySelectorAll(".player-icons"),
+        pcFields: document.querySelectorAll(".pc-icons"),
         pcSore: 0,
         playerScore: 0
     },
@@ -14,16 +15,31 @@ const game = {
         return Math.floor(Math.random() *3);
     },
 
-    winLooseCheck(fieldIndex) {
-        
+    pcIconDisplay(pcscore) {
+        this.data.pcFields.forEach(field => {
+            let index = parseInt(field.dataset.index);
+            if (index === pcscore) {
+                field.style.color = "white";
+            }
+        });
+    },
+
+    boardClear() {
+        setTimeout(() => {
+            this.data.pcFields.forEach(field => {
+                
+            });
+        }, 1000);
     },
 
     init() {
         this.data.playerFields.forEach(field =>  {
             field.addEventListener("click", () => {
                 this.data.pcSore = this.random();
+
+                this.pcIconDisplay(this.data.pcSore);
+                this.boardClear();
                 let fieldIndex = parseInt(field.dataset.index, 10);
-                this.winLooseCheck(fieldIndex);
             });
         });
     }
