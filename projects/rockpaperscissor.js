8cf283d6abfbd5a568686c9e622c3a2a63eec32d
Draszon <peti0202@windowslive.com>
2024-09-09 19:18:33 +0200

    haladás

--- projects/rockpaperscissor.js
+++ projects/rockpaperscissor.js
@@ -19,27 +19,39 @@ const game = {
         this.data.pcFields.forEach(field => {
             let index = parseInt(field.dataset.index);
             if (index === pcscore) {
-                field.style.color = "white";
+                field.style.color = "hsl(0, 0%, 100%)";
+                field.disabled = true;
+                setTimeout(() => {
+                    field.style.color = "hsl(0, 0%, 60%)";
+                    field.disabled = false;
+                }, 500);
             }
         });
     },
 
-    boardClear() {
-        setTimeout(() => {
-            this.data.pcFields.forEach(field => {
-                
-            });
-        }, 1000);
+    winLooseCheck(player, pc) {
+        if ((player === 0 && pc === 0) || (player === 1 && pc === 1) || player === 2 && pc === 2) {
+            return "Döntetlen!";
+        } else if ((player === 0 && pc === 1) || (player === 1 && pc === 2) || (player === 2 && pc === 0)) {
+            this.data.pcSore += 1;
+            return "PC Nyert!";
+        } else {
+            this.data.playerScore += 1;
+            return "Nyertél!";
+        }
     },
-
+    
     init() {
         this.data.playerFields.forEach(field =>  {
             field.addEventListener("click", () => {
                 this.data.pcSore = this.random();
-
+                this.data.playerScore = parseInt(field.dataset.index, 10);
                 this.pcIconDisplay(this.data.pcSore);
-                this.boardClear();
-                let fieldIndex = parseInt(field.dataset.index, 10);
+
+                this.data.result.textContent = this.winLooseCheck(this.data.playerScore, this.data.pcSore);
+                setTimeout(() => {
+                    this.data.result.textContent = "";
+                }, 500);
             });
         });
     }
